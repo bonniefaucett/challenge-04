d894b4cd87bf4e34b12f590b862b9f8b589e641d
Bonnie Faucett <bonniefaucett@Bonnies-iMac.local>
2023-01-26 20:24:31 -0700

    adding the quiz prompts

--- assets/script.js
+++ assets/script.js
@@ -36,10 +36,77 @@ questionWrapperEl.appendChild(quizInstructionsEl);
 var startButtonContainerEl = document.createElement("div");
 startButtonContainerEl.id = "startBtn-wrapper";
 
-//the start button calls initializeQuiz function when clicked, it is removed once the quiz starts
 var startButtonEl = document.createElement("button");
 startButtonEl.className = "btn";
 startButtonEl.id = "startBtn";
-startButtonEl.innerHTML = "Start Quiz";
+startButtonEl.innerHTML = "Start Quiz >";
 startButtonContainerEl.appendChild(startButtonEl);
 questionWrapperEl.appendChild(startButtonContainerEl);
+
+// The quiz prompts are stored in an array.
+var questionArray = [
+    {
+        text: "A for loop repeats until a specified condition evaluates to _______.",
+        option1: "true", // Correct answer
+        option2: "false",
+        option3: "truthy",
+        option4: "falsy",
+        answer: "1"
+    },
+    {
+        text: "JavaScript's String type is used to represent _______.",
+        option1: "an expression",
+        option2: "textual data", // Correct answer
+        option3: "an object",
+        option4: "an array",
+        answer: "2"
+    },
+    {
+        text: "When should you include a README file in your repository?",
+        option1: "when you feel like it",
+        option2: "when your boss asks you to",
+        option3: "never",
+        option4: "always", // Correct answer
+        answer: "4"
+    },
+    {
+        text: "What does CSS stand for?",
+        option1: "Cascading Style Sheets", // Correct answer
+        option2: "Certified Security Solutions",
+        option3: "Communications Support System",
+        option4: "Contact Start Stop",
+        answer: "1"
+    },
+    {
+        text: "Loops offer a quick and easy way to do something _______.",
+        option1: "at the top of every hour",
+        option2: "every minute",
+        option3: "repeatedly", // Correct answer
+        option4: "when I want it to",
+        answer: "3"
+    },
+    {
+        text: "Which of the following is not a primitive data type in JavaScript?",
+        option1: "object", // Correct answer
+        option2: "string",
+        option3: "symbol",
+        option4: "boolean",
+        answer: "1"
+    },
+    {
+        text: "NaN stands for _______.",
+        option1: "not a chance",
+        option2: "never in a nanosecond",
+        option3: "not at noon",
+        option4: "not a number", // Correct answer
+        answer: "4"
+    },
+    {
+        text: "What is GitHub?",
+        option1: "a social media site",
+        option2: "a code hosting platform for version control and collaboration", // Correct answer
+        option3: "a streaming service",
+        option4: "a ghost town",
+        answer: "2"
+    },
+];
